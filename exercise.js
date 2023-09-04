fa8e4c415ac9c83a85540158105a4a36f3cc308e
Britt <140755195+britthanlon10@users.noreply.github.com>
2023-09-04 19:11:38 -0230

    Create exercise.js

---
+++ exercise.js
@@ -0,0 +1,23 @@
+// exercise 1: Print “Number is even” if the number is divisible by 2. Else print “Number is odd” if the number returns a remainder when divided by 2.
+
+
+// exercise 2: How many times will it print “I love JavaScript “.
+
+const n = 5;
+for (let i = 1; i <= n; i++) {
+    console.log(`I love JavaScript.`);
+}
+
+// exercise 3: change this to a for loop.
+
+let f = 0;
+while (f > 2){
+    console.log (f)
+    f++
+}
+
+// exersice 4: change this to a while loop.
+
+for (let i = 4; i > 0; i--){
+    console.log (i);
+}
