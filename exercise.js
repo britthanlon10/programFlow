90c995843ade1840138f8c37160e6303f5ba0cc3
Britt <140755195+britthanlon10@users.noreply.github.com>
2023-09-05 12:31:48 -0230

    Update exercise 3 and 4

--- exercise.js
+++ exercise.js
@@ -1,4 +1,7 @@
-// exercise 1: Print “Number is even” if the number is divisible by 2. Else print “Number is odd” if the number returns a remainder when divided by 2.
+/* exercise 1: Print “Number is even” if the number is divisible by 2. 
+Else print “Number is odd” if the number returns a remainder when divided by 2. */
+
+
 
 
 
@@ -18,8 +21,17 @@ while (f > 2){
     f++
 }
 
+for (let f = 0; f > 2; f++)
+
 // exersice 4: change this to a while loop.
 
 for (let i = 4; i > 0; i--){
     console.log (i);
 }
+
+let i = 0;
+while (i > 0) {
+    console.log (i)
+    i--
+}]
+    
